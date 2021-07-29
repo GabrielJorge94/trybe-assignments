979ea70ad51cdd18e6f3a193e66c33ab2f32bb3c
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-07-29 17:20:14 -0400

    adiciona exercicio 03 e 04

--- 01-fundamentals/04-JavaScript-introduction/day-02-javascript-for-array/scripts-exercicio-01.js
+++ 01-fundamentals/04-JavaScript-introduction/day-02-javascript-for-array/scripts-exercicio-01.js
@@ -4,19 +4,31 @@
 //   console.log (numbers[index]);
 // }
 
-// Exercicio 2 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sumNumbers = 0;
-for (let i = 0 ; i < numbers.length; i++) {
-  sumNumbers += numbers[i];
-}
-console.log (sumNumbers);
-
-
-// const array = [1, 2, 3, 4];
-// let sum = 0;
-
-// for (let i = 0; i < array.length; i++) {
-//     sum += array[i];
+// // Exercicio 2 
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sumNumbers = 0;
+// for (let i = 0 ; i < numbers.length; i++) {
+//   sumNumbers += numbers[i];
 // }
-// console.log(sum);
+// console.log (sumNumbers);
+
+// Exercicio 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sumNumbers = 0;
+// for (let i = 0 ; i < numbers.length; i++) {
+//  averageNumbers = ((sumNumbers += numbers[i])/numbers.length);
+// }
+// console.log (averageNumbers);
+
+// Exercicio 4
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sumNumbers = 0;
+// for (let i = 0 ; i < numbers.length; i++) {
+//  averageNumbers = ((sumNumbers += numbers[i])/numbers.length);
+// }
+// if (averageNumbers > 20) {
+//   console.log ("Valor maior que 20");
+// }
+// else {
+//   console.log("valor menor ou igual a 20")
+// }
