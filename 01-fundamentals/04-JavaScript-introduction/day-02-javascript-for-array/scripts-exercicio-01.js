4bfa37f6f9711474f6e9867550ceed20fa290ac8
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-07-29 17:10:02 -0400

    adiciona exercicio 01

---
+++ 01-fundamentals/04-JavaScript-introduction/day-02-javascript-for-array/scripts-exercicio-01.js
@@ -0,0 +1,22 @@
+// // Exercicio 1  
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 0; index < numbers.length ; index += 1) {
+//   console.log (numbers[index]);
+// }
+
+// Exercicio 2 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumNumbers = 0;
+for (let i = 0 ; i < numbers.length; i++) {
+  sumNumbers += numbers[i];
+}
+console.log (sumNumbers);
+
+
+// const array = [1, 2, 3, 4];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+// }
+// console.log(sum);
