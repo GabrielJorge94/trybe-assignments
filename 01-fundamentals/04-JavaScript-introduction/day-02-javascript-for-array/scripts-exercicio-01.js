e185edc5ead928b03b98b449787aa851d8c768da
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-07-29 19:16:42 -0400

    adiciona exercicio 07

--- 01-fundamentals/04-JavaScript-introduction/day-02-javascript-for-array/scripts-exercicio-01.js
+++ 01-fundamentals/04-JavaScript-introduction/day-02-javascript-for-array/scripts-exercicio-01.js
@@ -44,11 +44,30 @@
 // console.log(biggestNumber);
 
 // Exercicio 6
-let numbers = [2,4,5,7,9];
-let oddNumber = [];
-for (index = 0; index < numbers.length; index += 1) {
-if (numbers[index] % 2 != 0) {
-  oddNumber.push(numbers[index]);
-} 
+// let numbers = [2,4];
+// let oddNumber = 0;
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 != 0) {
+//     oddNumber += 1;
+//   }
+// }
+
+// if (oddNumber === 0) {
+//   console.log("Nenhum numero impar");
+// }
+// else {
+//   console.log (oddNumber);
+// }
+
+
+
+// Exercicio 7
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smallestNumber = numbers[0];
+for (let i = 1 ; i < numbers.length; i++) {
+  if (numbers[i] < smallestNumber) {
+    smallestNumber = numbers [i];
+  }
 }
-console.log(oddNumber.length);
+console.log(smallestNumber);
