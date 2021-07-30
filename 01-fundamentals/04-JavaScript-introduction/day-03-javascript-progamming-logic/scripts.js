41a87392116441d578a1f7762fe9107ebd233af5
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-07-30 17:52:15 -0400

    adiciona exercicio 03

--- 01-fundamentals/04-JavaScript-introduction/day-03-javascript-progamming-logic/scripts.js
+++ 01-fundamentals/04-JavaScript-introduction/day-03-javascript-progamming-logic/scripts.js
@@ -5,19 +5,33 @@
 
 // criar variavel n
 
-let n = 5;
-// verificar condicao n maior que 1
-if (n > 1) {
-// cria for para repeticao lados do quadrado
-  for (i = 1 ; i <= n; i += 1) {
-    console.log('*'.repeat(n));
-  }
-}
+// let n = 5;
+// // verificar condicao n maior que 1
+// if (n > 1) {
+// // cria for para repeticao lados do quadrado
+//   for (i = 1 ; i <= n; i += 1) {
+//     console.log('*'.repeat(n));
+//   }
+// }
 
-// // Exercicio 02
+// Exercicio 02
 
+// criar variavel para o valor n;
+// se n > 1 entao imprime n 
+
+
+
+// Exercicio 03
 // criar variavel para valor para n;
+// let n = 5; 
 // n > 1 ;
+// if (n>1){
+  // cria variavel para incrementar n de 1 em 1;
+// imprime asteriscos do 1 para o n;
+// imprime espacos de n-1 para 1;
 
-
-
+  // for (i = 1 ; i <= n ; i += 1) {
+    // k = n - i
+    // console.log (" ".repeat(k) + "*".repeat(i));
+  // }  
+// }
