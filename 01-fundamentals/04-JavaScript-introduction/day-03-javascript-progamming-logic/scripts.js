0d1ca2c62b42451369550d0ab8b816af426419d6
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-07-30 17:03:36 -0400

    refatora exercicio 01

--- 01-fundamentals/04-JavaScript-introduction/day-03-javascript-progamming-logic/scripts.js
+++ 01-fundamentals/04-JavaScript-introduction/day-03-javascript-progamming-logic/scripts.js
@@ -1,3 +1,4 @@
+// Exercicio 01
 // criar valor para n
 // n maior que 1
 // imprimir um quadrado feito de * de lado = n
@@ -5,19 +6,18 @@
 // criar variavel n
 
 let n = 5;
-let asterico = "*";
 // verificar condicao n maior que 1
-
 if (n > 1) {
-
 // cria for para repeticao lados do quadrado
-
   for (i = 1 ; i <= n; i += 1) {
-    asterico = '*'.repeat(n);
-    console.log(asterico);
+    console.log('*'.repeat(n));
   }
 }
 
+// // Exercicio 02
+
+// criar variavel para valor para n;
+// n > 1 ;
 
 
 
