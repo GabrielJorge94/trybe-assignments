ad9ee99eadba452e4f4e9e420da4a402f54de561
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-02 15:06:34 -0400

    reafatora usando function

--- 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/scripts.js
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/scripts.js
@@ -1,21 +1,43 @@
-// Para fixar 01 - objetos
+// // Para fixar 01 - objetos
 
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3 
-  },
-  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   medals: {
+//     golden: 2,
+//     silver: 3 
+//   },
+//   bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// }
+
+// player.fullName = player.name + " " + player.lastName ;
+
+// console.log ("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata." );
+
+// Para fixar 02 - for/in 
+
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge' 
+// };
+
+// for ( let key in names) {
+//   console.log ("Ola " + names[key]);
+// }
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log (key, car[key]);
 }
-
-player.fullName = player.name + " " + player.lastName ;
-
-console.log ("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata." );
