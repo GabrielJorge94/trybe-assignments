799c9c4d395ff14caf1c64ba6914ad3ad3704db3
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-02 15:07:32 -0400

    adiciona para fixar 2 - 4.4

--- 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/scripts.js
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/scripts.js
@@ -32,12 +32,12 @@
 //   console.log ("Ola " + names[key]);
 // }
 
-let car = {
-  model: 'A3 Sedan',
-  manufacturer: 'Audi',
-  year: 2020
-};
+// let car = {
+//   model: 'A3 Sedan',
+//   manufacturer: 'Audi',
+//   year: 2020
+// };
 
-for (let key in car) {
-  console.log (key, car[key]);
-}
+// for (let key in car) {
+//   console.log (key, car[key]);
+// }
