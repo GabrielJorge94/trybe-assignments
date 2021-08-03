4aafd14172db1b31115df49fe823ab021aaf60cc
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-02 22:00:08 -0400

    adiciona exercicio parte 2.2

--- 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
@@ -17,3 +17,20 @@
 
 // console.log(verificaPalindrome("arara"));
 // console.log(verificaPalindrome("desenvolvimento"));
+
+
+// Exercicio 02 - 
+
+let teste = [2, 3, 6, 7, 10, 1];
+
+function maiorIndice () {
+  let maior = 0;
+  for ( i = 0 ; i < teste.length ; i += 1) {
+    if (teste[i] > teste[maior]) {
+      maior = i;
+    }
+    
+  }
+  return maior
+}
+console.log(maiorIndice (teste));
