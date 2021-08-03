27cc610c0d568bfd0b1345b52ed9cb797ed9e55e
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-02 21:34:22 -0400

    adiciona exercicio parte 2.1

---
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
@@ -0,0 +1,19 @@
+// Exercicio 01 - 
+
+// let palindrome = "arara";
+// let joinArray = "";
+// function verificaPalindrome(string) {
+//   var splitString = string.split (""); 
+//   var reverseArray = splitString.reverse();
+//   var joinArray = reverseArray.join("")
+
+//   if (joinArray === string) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+// console.log(verificaPalindrome("arara"));
+// console.log(verificaPalindrome("desenvolvimento"));
