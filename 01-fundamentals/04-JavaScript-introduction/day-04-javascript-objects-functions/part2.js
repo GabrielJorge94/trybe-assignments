59548875e3711244fca8aa54c7422810744b64b4
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-02 22:04:17 -0400

    adiciona exercicio parte 2.3

--- 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
@@ -21,16 +21,32 @@
 
 // Exercicio 02 - 
 
-let teste = [2, 3, 6, 7, 10, 1];
+// let teste = [2, 3, 6, 7, 10, 1];
+// 
+// function maiorIndice () {
+  // let maior = 0;
+  // for ( i = 0 ; i < teste.length ; i += 1) {
+    // if (teste[i] > teste[maior]) {
+      // maior = i;
+    // }
+    // 
+  // }
+  // return maior
+// }
+// console.log(maiorIndice (teste));
 
-function maiorIndice () {
-  let maior = 0;
+// Exercicio 03 - 
+
+let teste = [2, 4, 6, 7, 10, 0, -3];
+
+function menorIndice () {
+  let menor = 0;
   for ( i = 0 ; i < teste.length ; i += 1) {
-    if (teste[i] > teste[maior]) {
-      maior = i;
+    if (teste[i] < teste[menor]) {
+      menor = i;
     }
     
   }
-  return maior
+  return menor
 }
-console.log(maiorIndice (teste));
+console.log(menorIndice (teste));
