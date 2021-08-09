9ce73cc7f40ca100de091d16e45c3663d4b14ee4
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-09 15:04:36 -0300

    atualiza part2 dia 04

--- 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
+++ 01-fundamentals/04-JavaScript-introduction/day-04-javascript-objects-functions/part2.js
@@ -53,15 +53,51 @@
 
 // Exercicio 04 - 
 
-let teste =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-function maiorNome () {
-  let maisCaracteres = 0 ;
-  for (i = 0 ; i < teste.length ; i += 1) {
-    if (teste[i].length > teste[maisCaracteres].length)
-    maisCaracteres = i;
+// let teste =  ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+// function maiorNome () {
+//   let maisCaracteres = 0 ;
+//   for (i = 0 ; i < teste.length ; i += 1) {
+//     if (teste[i].length > teste[maisCaracteres].length)
+//     maisCaracteres = i;
     
-  }
-  return teste[maisCaracteres]
-}
+//   }
+//   return teste[maisCaracteres]
+// }
 
-console.log (maiorNome(teste))
+// console.log (maiorNome(teste))
+
+// Exercicio 05 - 
+
+// let teste = [2, 3, 2, 5, 8, 2, 3];
+// let numeroRepetido = 0 ;
+// // let vezesQueRepete = 0 ; 
+
+// function maisRepete () {
+//   let repete = 0;
+//   for (i = 0 ; i<teste.length ; i += 1) {
+//     if (teste[i] === repete) {
+//       repete = teste[i];
+//     }
+    
+//   }
+// }
+
+// Exercicio 6 - 
+
+// function somaN (num) {
+//   let k = 0;
+//   for (i = 1 ; i <= num ; i += 1) {
+//     k += i;
+//   }
+//   return k ;
+// }
+
+// console.log(somaN(10));
+
+
+// let exemplo1 = ['o', 'i']
+// let exemplo2 = exemplo1.reverse()
+
+// console.log(exemplo2)
+
+//Resultado: [ 'i', 'o' ]
