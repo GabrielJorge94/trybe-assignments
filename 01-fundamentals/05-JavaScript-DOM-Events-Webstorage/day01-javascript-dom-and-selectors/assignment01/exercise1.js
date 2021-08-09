1a5603e9791597b54800256baad0129918f309b6
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-09 18:29:48 -0300

    muda cor do quadrado vermelho para branco

--- 01-fundamentals/05-JavaScript-DOM-Events-Webstorage/day01-javascript-dom-and-selectors/assignment01/exercise1.js
+++ 01-fundamentals/05-JavaScript-DOM-Events-Webstorage/day01-javascript-dom-and-selectors/assignment01/exercise1.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName("p")[1].innerText = "muda o texto do p"
 
 document.querySelector(".main-content").style.backgroundColor = ("rgb(76,164,109");
+
+document.querySelector(".main-content .center-content").style.backgroundColor = "white";
