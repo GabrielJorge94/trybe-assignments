2d35e63cf18540cdd811a04f46830d62d14ce4d1
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-11 20:55:34 -0300

    add exercise 1&2

---
+++ 01-fundamentals/05-JavaScript-DOM-Events-Webstorage/day03-Javascript-Events/exercicios/script.js
@@ -0,0 +1,53 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+  
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+function createDaysOfTheMonth(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  
+  for(let i = 0; i < dezDaysList.length; i +=1){
+    let monthDay = dezDaysList[i]
+    let getUl = document.getElementById('days');
+    let dayOfTheMonth = document.createElement('li');
+    dayOfTheMonth.innerHTML = monthDay;
+    if(monthDay === 24|| monthDay === 31 ){
+      dayOfTheMonth.className ="day holiday"
+    }
+    else if(monthDay === 4|| monthDay === 11|| monthDay === 18) {
+      dayOfTheMonth.className ="day friday"
+    }
+    else if(monthDay === 25){
+      dayOfTheMonth.className ="day holiday friday"
+    }
+    else {
+    dayOfTheMonth.className ="day"}
+    getUl.appendChild(dayOfTheMonth)
+    
+    
+  }
+}
+createDaysOfTheMonth();
+
+function createButton(Feriados){
+  let addButton = document.createElement('button');
+  let getDiv = document.querySelector(".buttons-container");
+  addButton.innerText = "Feriados";
+  addButton.id = "btn-holiday";
+  getDiv.appendChild(addButton);
+
+}
+createButton('Feriados');
+
+// Escreva seu código abaixo.
