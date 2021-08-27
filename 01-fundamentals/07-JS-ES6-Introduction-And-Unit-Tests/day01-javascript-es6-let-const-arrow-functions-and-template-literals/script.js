4ebc9aebba92f1ae43bcba593402507ec1bc4dfe
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-27 18:15:32 -0300

    adiciona part 2

--- 01-fundamentals/07-JS-ES6-Introduction-And-Unit-Tests/day01-javascript-es6-let-const-arrow-functions-and-template-literals/script.js
+++ 01-fundamentals/07-JS-ES6-Introduction-And-Unit-Tests/day01-javascript-es6-let-const-arrow-functions-and-template-literals/script.js
@@ -1,12 +1,39 @@
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-    console.log(ifScope);
-  } else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
+// part1 - 1
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+//     console.log(ifScope);
+//   } else {
+//     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+// part 1 - 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens.sort((a, b) => a - b);
+// console.log(oddsAndEvens);
+
+// part 2 - 1
+const factoral = (number) => {
+  let resultado = number;
+  for (let i = 1; i < number; i += 1) {
+    resultado *= i;
   }
+  return resultado
 }
 
-testingScope(true);
+// console.log(factoral(5));
+
+// part 2 - 2
+// longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+const longestWord = (phrase) => {
+  const get = phrase.split(' ');
+  console.log(get);
+}
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu");
