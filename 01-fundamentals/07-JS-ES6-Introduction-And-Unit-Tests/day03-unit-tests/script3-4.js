df58a42792db221c83b81a2e2817541ca0a9efcc
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-31 23:09:43 -0300

    add 3-4

---
+++ 01-fundamentals/07-JS-ES6-Introduction-And-Unit-Tests/day03-unit-tests/script3-4.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+function secondThirdSmallest(array) {
+  let results = []
+  array.sort(function (x, y) {
+      return x - y;
+  });
+  results = [array[1], array[2]];
+  return results
+};
+
+const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+const result = [5, 6];
+const expected = secondThirdSmallest(parameter)
+assert.deepStrictEqual(expected, result, 'deve retornar o resultado');
