6d9726988d6483b76b19a21919312d51849300c7
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-31 21:07:06 -0300

    add 3.4

--- 01-fundamentals/07-JS-ES6-Introduction-And-Unit-Tests/day03-unit-tests/script03.js
+++ 01-fundamentals/07-JS-ES6-Introduction-And-Unit-Tests/day03-unit-tests/script03.js
@@ -19,5 +19,8 @@ assert.deepStrictEqual(expected, [1,2,4], 'retorna o array esperado');
 assert.notDeepStrictEqual(expected, [1,2,3,4], 'não retorna o array [1, 2, 3, 4]');
 // Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
 const array = [7,8,9];
-myRemoveWithoutCopy(array,10);
+myRemoveWithoutCopy(array,8);
 assert.notDeepStrictEqual(array, [7,8,9], 'array nao sofreu alteracao');
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+const expected2 = myRemoveWithoutCopy([1,2,3,4], 5);
+assert.deepStrictEqual(expected2, [1,2,3,4], 'deve retornar o array [1,2,3,4]');
