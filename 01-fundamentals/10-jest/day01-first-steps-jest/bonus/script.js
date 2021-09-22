18093658d650d716957359879e1103d347c25ba0
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-21 21:17:28 -0400

    add bonus

---
+++ 01-fundamentals/10-jest/day01-first-steps-jest/bonus/script.js
@@ -0,0 +1,53 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  const findEmployee = professionalBoard.find((employee) => employee.id === id);
+  if(!findEmployee){
+    throw new Error('ID não identificada');
+  }
+  if(!findEmployee[detail]){
+    throw new Error('Informação indisponível');
+  }
+  return findEmployee[detail]
+};
+
+module.exports ={ searchEmployee , professionalBoard}
