dfa6589ab11b482f181e818f11dfe1eea274131d
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-22 17:56:08 -0400

    add tests to uppercase.test.js

--- 01-fundamentals/10-jest/day02-jest-assync-test/uppercase.test.js
+++ 01-fundamentals/10-jest/day02-jest-assync-test/uppercase.test.js
@@ -3,3 +3,14 @@ const uppercase = (str, callback) => {
     callback(str.toUpperCase());
   }, 500);
 };
+
+test('verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas.', (done) => {
+  uppercase('ola', (result) => {
+    try {
+      expect(result).toBe('OLA');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
