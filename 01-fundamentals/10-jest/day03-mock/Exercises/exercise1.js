278db7ffee597ff6b14c3216c738b94dc9fe55b7
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-09-23 17:34:20 -0400

    add exercise1.js and exercise1.test.js

---
+++ 01-fundamentals/10-jest/day03-mock/Exercises/exercise1.js
@@ -0,0 +1,7 @@
+// Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * (100 - 0));
+};
+
+module.exports = { generateRandomNumber };
