338dddf6b2d2b2dd16c7f0fa7ff0199700c0af1d
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-11-03 18:49:12 -0400

    Add exercise 2

--- 02-front-end/14-automated-tests-react-testing-library/02-rtl-mocks-and-inputs/service.test.js
+++ 02-front-end/14-automated-tests-react-testing-library/02-rtl-mocks-and-inputs/service.test.js
@@ -1,6 +1,11 @@
+const { afterEach } = require('@jest/globals');
 const service = require('./service');
 
 describe('Testa funcao que gera nuymero aleatorio', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('Verifica se a funcao foi chamada, qual o valor retornado e quantas vezes foi chamada', () => {
     service.generateRandomNumber = jest.fn();
 
@@ -11,6 +16,18 @@ describe('Testa funcao que gera nuymero aleatorio', () => {
     service.generateRandomNumber = jest.fn().mockReturnValue(10);
 
     expect(service.generateRandomNumber()).toBe(10);
+
+    expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  it('Teste se a função foi chamada e a nova implementação de divisão foi aplicada.', () => {
+    service.generateRandomNumber = jest.fn().mockImplementation((num1, num2) => num1 / num2);
+
+    expect(service.generateRandomNumber(10, 2)).toBe(5);
+
+    expect(service.generateRandomNumber).toHaveBeenCalled();
+
+    expect(service.generateRandomNumber).toHaveBeenCalledWith(10, 2);
 
     expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
   });
