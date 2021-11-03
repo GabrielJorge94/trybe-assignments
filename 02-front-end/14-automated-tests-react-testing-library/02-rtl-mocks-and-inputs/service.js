03b9cbec112e37c7ef2b0a0df5503f8ba31a579d
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-11-03 18:31:04 -0400

    add first exercise

---
+++ 02-front-end/14-automated-tests-react-testing-library/02-rtl-mocks-and-inputs/service.js
@@ -0,0 +1,5 @@
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+console.log(generateRandomNumber());
+
+module.exports = generateRandomNumber;
--- 02-front-end/14-automated-tests-react-testing-library/02-rtl-mocks-and-inputs/service.test.js
+++ 02-front-end/14-automated-tests-react-testing-library/02-rtl-mocks-and-inputs/service.test.js
@@ -1,1 +1,17 @@
-  
+const service = require('./service');
+
+describe('Testa funcao que gera nuymero aleatorio', () => {
+  it('Verifica se a funcao foi chamada, qual o valor retornado e quantas vezes foi chamada', () => {
+    service.generateRandomNumber = jest.fn();
+
+    service.generateRandomNumber();
+
+    expect(service.generateRandomNumber).toHaveBeenCalled();
+
+    service.generateRandomNumber = jest.fn().mockReturnValue(10);
+
+    expect(service.generateRandomNumber()).toBe(10);
+
+    expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
+  });
+});
